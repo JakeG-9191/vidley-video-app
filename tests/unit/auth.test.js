5add77cdb661ac5d37de6d9bfe355fbdd5a1d274
Jake Garlick <jacobgarlick@msn.com>
2020-01-06 17:04:32 -0700

    additional unit testing for middleware authorization

---
+++ tests/unit/auth.test.js
@@ -0,0 +1,22 @@
+const { Register } = require('../../models/register');
+const auth = require('../../middleware/auth');
+const mongoose = require('mongoose');
+
+describe('auth middleware', () => {
+  it('should populate req.user with payload of valid JWT', () => {
+    const user = {
+      _id: mongoose.Types.ObjectId().toHexString(),
+      isAdmin: true
+    };
+    const token = new Register(user).generateAuthToken();
+    const req = {
+      header: jest.fn().mockReturnValue(token)
+    };
+    const res = {};
+    const next = jest.fn();
+
+    auth(req, res, next);
+
+    expect(req.user).toMatchObject(user);
+  });
+});
