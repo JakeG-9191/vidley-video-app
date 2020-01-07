6eb52be50306e8fa2a24cbdde46be50cc6ee9968
Jake Garlick <jacobgarlick@msn.com>
2020-01-07 15:48:51 -0700

    refactored code in return route, update rental model for better reusability, created new validate middleware

--- routes/returns.js
+++ routes/returns.js
@@ -1,20 +1,13 @@
-const moment = require('moment');
+const Joi = require('joi');
+const validate = require('../middleware/validate');
 const { Rental } = require('../models/rental');
 const { Movie } = require('../models/movie');
 const auth = require('../middleware/auth');
 const express = require('express');
 const router = express.Router();
 
-router.post('/', auth, async (req, res) => {
-  if (!req.body.customerId)
-    return res.status(400).send('CustomerId not provided');
-  if (!req.body.movieId)
-    return res.status(400).send('MovieId was not provided');
-
-  const rental = await Rental.findOne({
-    'customer._id': req.body.customerId,
-    'movie._id': req.body.movieId
-  });
+router.post('/', [auth, validate(validateReturn)], async (req, res) => {
+  const rental = await Rental.lookup(req.body.customerId, req.body.movieId);
 
   if (!rental)
     return res.status(404).send('No rental found for customer or movie');
@@ -22,9 +15,7 @@ router.post('/', auth, async (req, res) => {
   if (rental.dateReturned)
     return res.status(400).send('This rental return has already processed');
 
-  rental.dateReturned = new Date();
-  const rentalDays = moment().diff(rental.dateOut, 'days');
-  rental.rentalFee = rentalDays * rental.movie.dailyRentalRate;
+  rental.return();
   await rental.save();
 
   await Movie.update(
@@ -34,7 +25,15 @@ router.post('/', auth, async (req, res) => {
     }
   );
 
-  return res.status(200).send(rental);
+  return res.send(rental);
 });
+
+function validateReturn(req) {
+  const schema = {
+    customerId: Joi.objectId().required(),
+    movieId: Joi.objectId().required()
+  };
+  return Joi.validate(req, schema);
+}
 
 module.exports = router;
