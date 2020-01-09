f325290afe1e9944fe837d97593a56b88e76f2fa
Jake Garlick <jacobgarlick@msn.com>
2020-01-09 12:14:30 -0700

    changes to production startup file in order to prepare for heroku deployment

--- startup/logging.js
+++ startup/logging.js
@@ -3,7 +3,15 @@ const winston = require('winston');
 require('express-async-errors');
 
 module.exports = function() {
+  const logFormat = winston.format.printf(function(info) {
+    return `${date}-${info.level}: ${JSON.stringify(info.message, null, 4)}\n`;
+  });
+
   winston.exceptions.handle(
+    new winston.transports.Console({
+      level: 'info',
+      format: winston.format.combine(winston.format.colorize(), logFormat)
+    }),
     new winston.transports.File({ filename: 'uncaughtExceptions.log' })
   );
 
